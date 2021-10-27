4dfe130b515ef065320878dff7cd5bb0762829e6
markyul <pig052656@naver.com>
2021-10-27 19:46:27 +0900

    Add Toolbar
    toolbar에 menu icon 추가

--- app/src/main/java/com/example/youandidiary/MainActivity.kt
+++ app/src/main/java/com/example/youandidiary/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.youandidiary
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+
+        return true
     }
 }
