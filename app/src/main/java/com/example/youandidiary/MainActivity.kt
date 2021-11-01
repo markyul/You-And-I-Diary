b5f95629fd8a961cdf71e6bacc07dd997587fb43
MakRyul <pig052656@naver.com>
2021-11-01 20:24:16 +0900

    androidx로 Refactor

--- app/src/main/java/com/example/youandidiary/MainActivity.kt
+++ app/src/main/java/com/example/youandidiary/MainActivity.kt
@@ -1,21 +1,19 @@
 package com.example.youandidiary
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.Toolbar
+import androidx.appcompat.widget.Toolbar
 import android.view.Menu
 import android.widget.CalendarView
 
 class MainActivity : AppCompatActivity() {
     lateinit var toolbar: Toolbar
-    lateinit var calendar: CalendarView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         toolbar = findViewById(R.id.toolbar)
-        calendar = findViewById(R.id.calendar)
 
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
