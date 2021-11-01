f5acc29904c379ecbd112f368a3adc901cb70266
markyul <pig052656@naver.com>
2021-11-01 21:32:35 +0900

    material-calendar 구현
    주말 색 지정

--- app/src/main/java/com/example/youandidiary/MainActivity.kt
+++ app/src/main/java/com/example/youandidiary/MainActivity.kt
@@ -1,27 +1,60 @@
 package com.example.youandidiary
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.style.ForegroundColorSpan
 import androidx.appcompat.widget.Toolbar
 import android.view.Menu
-import android.widget.CalendarView
+import com.prolificinteractive.materialcalendarview.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var toolbar: Toolbar
+    lateinit var calendar: MaterialCalendarView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         toolbar = findViewById(R.id.toolbar)
+        calendar = findViewById(R.id.calendar)
 
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        calendar.addDecorators(SundayDecorator(), SaturdayDecorator())
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar_menu, menu)
 
         return true
+    }
+}
+
+class SundayDecorator: DayViewDecorator {
+    private var calendar = Calendar.getInstance()
+    override fun shouldDecorate(day: CalendarDay?): Boolean {
+        day?.copyTo(calendar)
+        val weekDay = calendar.get(Calendar.DAY_OF_WEEK)
+        return weekDay == Calendar.SUNDAY
+    }
+
+    override fun decorate(view: DayViewFacade?) {
+        view?.addSpan(object: ForegroundColorSpan(Color.RED){})
+    }
+}
+
+class SaturdayDecorator: DayViewDecorator {
+    private var calendar = Calendar.getInstance()
+    override fun shouldDecorate(day: CalendarDay?): Boolean {
+        day?.copyTo(calendar)
+        val weekDay = calendar.get(Calendar.DAY_OF_WEEK)
+        return weekDay == Calendar.SATURDAY
+    }
+
+    override fun decorate(view: DayViewFacade?) {
+        view?.addSpan(object: ForegroundColorSpan(Color.BLUE){})
     }
 }
