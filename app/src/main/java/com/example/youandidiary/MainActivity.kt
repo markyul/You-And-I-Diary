6c17277f7f5e3f203f5708b90f7348f4d21eb564
MakRyul <pig052656@naver.com>
2021-11-09 21:59:59 +0900

    event시 날짜에 표시
    - 임시로 클릭한 날짜에 표시하는 함수 생성
    - 당일 색과 크기 변경

--- app/src/main/java/com/example/youandidiary/MainActivity.kt
+++ app/src/main/java/com/example/youandidiary/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.youandidiary
 
+import android.content.Context
 import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
 import androidx.appcompat.widget.Toolbar
 import android.view.Menu
+import androidx.core.content.ContextCompat
 import com.prolificinteractive.materialcalendarview.*
+import com.prolificinteractive.materialcalendarview.spans.DotSpan
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var context = this;
+
         toolbar = findViewById(R.id.toolbar)
         calendar = findViewById(R.id.calendar)
 
@@ -24,8 +32,18 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
         val today = CalendarDay.today()
-        calendar.addDecorators(SundayDecorator(), SaturdayDecorator(), TodayDecorator(today))
+        calendar.addDecorators(SundayDecorator(), SaturdayDecorator(), TodayDecorator(today, context))
         calendar.selectedDate = today
+
+        calendar.setOnDateChangedListener(object: OnDateSelectedListener {
+            override fun onDateSelected(
+                widget: MaterialCalendarView,
+                date: CalendarDay,
+                selected: Boolean
+            ) {
+                calendar.addDecorator(EventDecorator(Collections.singleton(date), context))
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -61,13 +79,30 @@ class SaturdayDecorator: DayViewDecorator {
     }
 }
 
-class TodayDecorator(today: CalendarDay): DayViewDecorator {
+class TodayDecorator(today: CalendarDay, context: Context): DayViewDecorator {
     var myDay = today
+    var this_context = context
+
     override fun shouldDecorate(day: CalendarDay?): Boolean {
         return day == myDay
     }
 
     override fun decorate(view: DayViewFacade?) {
-        view?.addSpan(object: ForegroundColorSpan(Color.parseColor("#FF018786")){})
+        view?.addSpan(object: StyleSpan(Typeface.BOLD){})
+        view?.addSpan(object: RelativeSizeSpan(1.2f){})
+        view?.addSpan(object: ForegroundColorSpan(ContextCompat.getColor(this_context, R.color.teal_700)){})
+    }
+}
+
+class EventDecorator(dates: Collection<CalendarDay>, context: Context): DayViewDecorator {
+    var dates: HashSet<CalendarDay> = HashSet(dates)
+    var myContext = context
+
+    override fun shouldDecorate(day: CalendarDay?): Boolean {
+        return dates.contains(day)
+    }
+
+    override fun decorate(view: DayViewFacade?) {
+        view?.addSpan(DotSpan(5F, ContextCompat.getColor(myContext, R.color.teal_700)))
     }
 }
